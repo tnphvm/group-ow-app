a81f211b6f3bb37061735f5a74734f424a619e68
Jaynee Pham <tpham.jn@gmail.com>
2017-08-18 01:26:25 -0700

    Added more features to search view

--- app/src/main/java/com/example/jaynee/group_ow/SearchFragment.java
+++ app/src/main/java/com/example/jaynee/group_ow/SearchFragment.java
@@ -5,13 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class SearchFragment extends Fragment
 {
+   private Spinner serverList;
+   private Spinner modeList;
+
    public SearchFragment()
    {
-      // Required empty public constructor
    }
 
    @Override
@@ -20,11 +26,54 @@ public class SearchFragment extends Fragment
       super.onCreate(savedInstanceState);
    }
 
+   public class ItemSelectedListener implements AdapterView.OnItemSelectedListener
+   {
+      String firstItem = String.valueOf(serverList.getSelectedItem());
+
+      public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+      {
+         if (firstItem.equals(String.valueOf(serverList.getSelectedItem())))
+         {
+            // ToDo when first item is selected
+         } else
+         {
+            Toast.makeText(parent.getContext(),
+                    "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                    Toast.LENGTH_LONG).show();
+            // Todo when item is selected by the user
+         }
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> arg)
+      {
+      }
+   }
+
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                             Bundle savedInstanceState)
    {
       // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.search_fragment, container, false);
+      View mainView = inflater.inflate(R.layout.search_fragment, container, false);
+
+      serverList = (Spinner) mainView.findViewById(R.id.server_list);
+      serverList.setOnItemSelectedListener(new ItemSelectedListener());
+
+      modeList = (Spinner) mainView.findViewById(R.id.mode_list);
+      modeList.setOnItemSelectedListener(new ItemSelectedListener());
+
+      // Set spinner custom layout
+      ArrayAdapter serverAdapter = ArrayAdapter.createFromResource(getContext(), R.array.server_array,
+              R.layout.spinner_item);
+      serverAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+      serverList.setAdapter(serverAdapter);
+
+      ArrayAdapter modeAdapter = ArrayAdapter.createFromResource(getContext(), R.array.mode_array,
+              R.layout.spinner_item);
+      modeAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+      modeList.setAdapter(modeAdapter);
+
+      return mainView;
    }
 }
